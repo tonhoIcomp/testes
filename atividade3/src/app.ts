bd76fc30ff7c1b079a1e870c395c68b4646f4c01
Antonio J. Fernandes Neto <142276460+tonhoIcomp@users.noreply.github.com>
2023-10-25 13:05:46 -0400

    atividade 3

---
+++ atividade3/src/app.ts
@@ -0,0 +1,36 @@
+import express from 'express';
+import { PrismaClient } from '@prisma/client';
+import { Cliente } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+
+app.use(express.json());
+
+// Rota para servir o formulário HTML
+  app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Altere o caminho para o seu arquivo HTML
+});
+
+// Rotas para criar um novo Cliente
+app.post('/api/clientes', async (req, res) => {
+  try {
+    const { nome_completo, cpf, numero_celular, email, data_nascimento } = req.body;
+    const cliente = await prisma.cliente.create({
+      data: { nome_completo, cpf, numero_celular, email, data_nascimento },
+    });
+    res.json(cliente);
+  } catch (error) {
+   
+    res.status(500).json({ error: 'Erro ao criar um cliente.' });
+  }
+});
+
+// Rotas para outras operações CRUD (atualizar, excluir, listar) para todas as entidades
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
